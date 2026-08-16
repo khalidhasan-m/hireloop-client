cfc380c56f2ad2b3b3817546a266e3ac63740054
Khalid Hasan <khalidhasanmeskat@gmail.com>
2026-08-17 00:44:01 +0600

    auth added with database

---
+++ src/lib/auth.js
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGO_DB_URI);
+const db = client.db(process.env.AUTH_DB_NAME);
+
+export const auth = betterAuth({
+  //...other options
+  emailAndPassword: {
+    enabled: true,
+  },
+  // socialProviders: {
+  //   github: {
+  //     clientId: process.env.GITHUB_CLIENT_ID,
+  //     clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  //   },
+  // },
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client,
+  }),
+});
